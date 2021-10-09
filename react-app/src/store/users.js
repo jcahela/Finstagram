f89db157cddc916d5b5bbb97b6935a54b895d5bc
jasonc <jpacahela@gmail.com>
2021-10-09 17:47:10 -0500

    added users fetch to redux state that occurs every time the ProtectedRoute component loads

---
+++ react-app/src/store/users.js
@@ -0,0 +1,35 @@
+// constants
+const GET_USERS = 'users/GET_USERS';
+
+const getUsers = (users) => ({
+    type: GET_USERS,
+    payload: users
+})
+
+const initialState = { };
+
+export const getUsersThunk = () => async (dispatch) => {
+    const response = await fetch('api/users')
+
+    if (response.ok) {
+        const users = await response.json();
+        await dispatch(getUsers(users));
+        return null
+    }
+}
+
+export function usersReducer(state = initialState, action) {
+    let newState = {...state}
+    switch (action.type) {
+        case GET_USERS:
+            const users = action.payload;
+            const usersObj = {};
+            users.users.forEach(user => usersObj[user.id] = user)
+            newState = usersObj;
+            return newState
+        default:
+            return state
+    }
+}
+
+export default usersReducer;

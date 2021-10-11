f71c03a61b12134080d613caeb446647f49f582c
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-11 15:51:14 -0700

    next layer of styling applied

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -32,37 +32,42 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin} id='login-form'>
-      <div id="finstagram-logo">Finstagram</div>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div id='login-email-div'>
-        <input
-          name='email'
-          type='text'
-          aria-label='Email'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div id='login-password-div'>
-        <input
-          name='password'
-          type='password'
-          aria-label='Password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-      </div>
-      <div id='login-button-div'>
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div id='login-form-container'>
+      <form onSubmit={onLogin} id='login-form'>
+        <div id="finstagram-logo">Finstagram</div>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div id='login-email-div' className='outer-form-ele'>
+          <input
+            className='inner-form-ele'
+            name='email'
+            type='text'
+            aria-label='Email'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div id='login-password-div' className='outer-form-ele'>
+          <input
+            className='inner-form-ele'
+            name='password'
+            type='password'
+            aria-label='Password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+          <button>Show</button>
+        </div>
+        <div id='login-button-div' className='outer-form-ele'>
+          <button className='inner-form-ele' type='submit'>Log In</button>
+        </div>
+      </form>
+    </div>
   );
 };
 

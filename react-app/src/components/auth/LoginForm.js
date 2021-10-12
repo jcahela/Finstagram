772cd5ac24840d06137fedb4b1433decfce3becc
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-12 01:38:21 -0700

    Had to account for the following when styling: autofill, error <ul>. Made room for other components on login page.

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -11,6 +11,7 @@ const LoginForm = () => {
   const [password, setPassword] = useState('');
   const [toggleState, setToggleState] = useState(false);
   const [btnText, setBtnTxt] = useState('Show');
+  const [btnVisibility, setBtnVisibility] = useState('visibility-button-invisible');
   const [passwordVisibility, setPasswordVisibility] = useState('password');
 
   const user = useSelector(state => state.session.user);
@@ -40,7 +41,10 @@ const LoginForm = () => {
   };
 
   const updatePassword = (e) => {
-    setPassword(e.target.value);
+    const tVal = e.target.value;
+    setPassword(tVal);
+    if (tVal.length) setBtnVisibility('visibility-button-visible');
+    else setBtnVisibility('visibility-button-invisible');
   };
 
   if (user) {
@@ -48,43 +52,46 @@ const LoginForm = () => {
   }
 
   return (
-    <div id='login-page'>
-      <div id='login-form-container'>
-        <form onSubmit={onLogin} id='login-form'>
-          <div id="finstagram-logo-div"><span id='finstagram-logo'>Finstagram</span></div>
-          <div id='login-email-div' className='outer-form-ele'>
-            <input
-              className='inner-form-ele'
-              name='email'
-              type='text'
-              aria-label='Email'
-              placeholder='Email'
-              value={email}
-              onChange={updateEmail}
-            />
-          </div>
-          <div id='login-password-div' className='outer-form-ele'>
-            <input
-              className='inner-form-ele'
-              name='password'
-              type={passwordVisibility}
-              aria-label='Password'
-              placeholder='Password'
-              value={password}
-              onChange={updatePassword}
-            />
-          </div>
-          <button type='button' onClick={visibilityHandler} id='visibilityButton'>{btnText}</button>
-          <div id='login-button-div'>
-            <button id='login-button' className='inner-form-ele' type='submit'>Log In</button>
-          </div>
-          <div id='login-errors-div'>
-            {errors.map((error, ind) => (
-              <div key={ind}>{error}</div>
-            ))}
-          </div>
-        </form>
-      </div>
+    <div id='login-page' className='auth-page'>
+      <form onSubmit={onLogin} id='login-form' autoComplete='off'>
+        <div id="finstagram-logo-div"><span id='finstagram-logo'>Finstagram</span></div>
+        <div id='login-email-div' className='outer-form-ele'>
+          <input
+            className='inner-form-ele'
+            name='email'
+            type='text'
+            aria-label='Email'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div id='login-password-div' className='outer-form-ele'>
+          <input
+            className='inner-form-ele'
+            name='password'
+            type={passwordVisibility}
+            aria-label='Password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+        <button
+          type='button'
+          onClick={visibilityHandler}
+          id={btnVisibility}
+          display='none'
+        >{btnText}</button>
+        <div id='login-button-div'>
+          <button id='login-button' className='inner-form-ele' type='submit'>Log In</button>
+        </div>
+        <div id='login-errors-div'>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+      </form>
     </div>
   );
 };

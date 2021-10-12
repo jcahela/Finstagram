b927caf7bffc24a297f3d6737ead45cbe49b319c
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-12 14:22:19 -0700

    Warning hunted down

--- react-app/src/components/ExplorePage.js
+++ react-app/src/components/ExplorePage.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { getNonFollowedPostsThunk } from '../store/nonFollowedUsersPosts';
 import { useDispatch, useSelector } from 'react-redux';
 

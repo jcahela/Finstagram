5647d8bbaadff81cba6412868ce51da109e44d4c
jasonc <jpacahela@gmail.com>
2021-10-12 19:02:49 -0500

    changed link to /signup on landing page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -126,7 +126,7 @@ const LoginForm = () => {
             </div>
           </form>
           <div className="link-to-signup-container">
-                Don't have an account? <span><Link id="signup-link" className="signup-link" to="/sign-up">Sign up here</Link></span>
+                Don't have an account? <span><Link id="signup-link" className="signup-link" to="/signup">Sign up here</Link></span>
           </div>
         </div>
       </div>

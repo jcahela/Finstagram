b2eb4e56fe13b7f4307887a286adb16d854d50da
Josue Lugaro <josuelugaro15@gmail.com>
2021-10-13 18:26:27 -0500

    Added user name and picture to explore modal

--- react-app/src/components/ExplorePostDetails.js
+++ react-app/src/components/ExplorePostDetails.js
@@ -18,6 +18,10 @@ function ExplorePostDetails({postKey, posts}) {
                 <img src={posts[postKey].content} className="detail-image" alt="This is something"/>
             </div>
             <div className="details">
+                <div className="user-info">
+                    <img src={users[posts[postKey].user_id].profile_picture} className="profile-pic" alt="this is something"/>
+                    <p className="user-name">{users[posts[postKey].user_id].firstname} {users[posts[postKey].user_id].lastname}</p> <span>•</span> <span className="explore-follow">Follow</span>
+                </div>
                 <p>stuff</p>
             </div>
         </div>

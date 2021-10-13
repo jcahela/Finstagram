6516c479363329b4e5852763b866ad8dcb8d9888
jasonc <jpacahela@gmail.com>
2021-10-12 22:47:15 -0500

    finished carousel on landing page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import { getSessionUsersPostsThunk } from '../../store/sessionUserPosts';
 import Footer from '../Footer/Footer';
 import { Link } from 'react-router-dom';
+import { Fade } from 'react-slideshow-image'
+import "react-slideshow-image/dist/styles.css"
 import './auth_css/Auth.css';
 import './auth_css/LoginForm.css';
 import './Landing.css'
@@ -22,9 +24,21 @@ const LoginForm = () => {
   const [innerEleTheme, setInnerEleTheme] = useState('inner-form-ele');
   const [outerEleTheme, setOuterEleTheme] = useState('outer-login-form-ele');
   const [visBtnPos, setVisBtnPos] = useState('skewed-left');
-
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+
+  const carouselImages = [
+    'https://cdn.discordapp.com/attachments/886336420552269847/897666081974521856/unknown.png',
+    'https://cdn.discordapp.com/attachments/886336420552269847/897665208066134087/unknown.png',
+    'https://cdn.discordapp.com/attachments/886336420552269847/897666379401023508/unknown.png',
+    'https://cdn.discordapp.com/attachments/886336420552269847/897666832004173824/unknown.png',
+    'https://cdn.discordapp.com/attachments/886336420552269847/897666972538523668/unknown.png'
+  ]
+
+  useEffect(() => {
+    const arrows = document.querySelectorAll('button.default-nav');
+    arrows.forEach(arrow => arrow.style.display = "none")
+  }, [])
 
   const themeToggleHandler = (e) => {
     if (!toggleTheme) {
@@ -60,8 +74,17 @@ const LoginForm = () => {
     if (data) {
       setErrors(data);
     }
-    await dispatch(getSessionUsersPostsThunk())
+    await dispatch(getSessionUsersPostsThunk());
   };
+
+  const loginAsDemo = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login("demo@aa.io", "password"));
+    if (data) {
+      setErrors(data);
+    }
+    await dispatch(getSessionUsersPostsThunk());
+  }
 
   const updateEmail = (e) => setEmail(e.target.value);
 
@@ -79,7 +102,27 @@ const LoginForm = () => {
   return (
     <div className='auth-page'>
       <div className="login-content-container">
-        <div className="carousel-holder" style={{backgroundImage:"url('https://cdn.discordapp.com/attachments/886336420552269847/897622826859773972/landing-carousel-holder.png')"}}></div>
+        <div className="carousel-holder" style={{backgroundImage:"url('https://cdn.discordapp.com/attachments/886336420552269847/897622826859773972/landing-carousel-holder.png')"}}>
+          <div className="slide-container carousel">
+            <Fade>
+              <div className="each-fade">
+                <img className="carousel-image" src={carouselImages[0]} alt="carousel" />
+              </div>
+              <div className="each-fade">
+                <img className="carousel-image" src={carouselImages[1]} alt="carousel" />
+              </div>
+              <div className="each-fade">
+                <img className="carousel-image" src={carouselImages[2]} alt="carousel" />
+              </div>
+              <div className="each-fade">
+                <img className="carousel-image" src={carouselImages[3]} alt="carousel" />
+              </div>
+              <div className="each-fade">
+                <img className="carousel-image" src={carouselImages[4]} alt="carousel" />
+              </div>
+            </Fade>
+          </div>
+        </div>
         <div className="form-and-link-container">
           <form onSubmit={onLogin} id='login-form' className={easterEgg} autoComplete='off'>
             <div
@@ -124,6 +167,10 @@ const LoginForm = () => {
                 <div key={ind}>{error}</div>
               ))}
             </div>
+            <div className="or-container">            
+              <div className="or-divider" /> <span className="or-text">OR</span> <div className="or-divider" />
+            </div>
+            <span onClick={loginAsDemo} className="demo-button">Login as a Demo User</span>
           </form>
           <div className="link-to-signup-container">
                 Don't have an account? <span><Link id="signup-link" className="signup-link" to="/signup">Sign up here</Link></span>

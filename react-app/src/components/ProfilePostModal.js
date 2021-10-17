9e44b3e5452bd88f87eefc703be622ddd262d5ef
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-17 13:34:54 -0700

    send email input gained toggle display functionality in redux state

--- react-app/src/components/ProfilePostModal.js
+++ react-app/src/components/ProfilePostModal.js
@@ -5,10 +5,22 @@ import './ProfilePostModal.css';
 const SharePostModal = ({ post, user, isVideo }) => {
     const emailInputRef = useRef(null);
     const { closeModal } = useModal();
+    const [toggleState, setToggleState] = useState(false);
     const [visibility, setVisibility] = useState('invisible');
+    const [expanded, setExpanded] = useState('expanded');
+    const [emailSpanText, setEmailSpanText] = useState('Share via Email');
 
-    const sharePost = (e) => {
-        setVisibility('');
+    const emailToggler = (e) => {
+        if (!toggleState) {
+            setVisibility('');
+            setExpanded('');
+            setEmailSpanText('Send to');
+        } else {
+            setVisibility('invisible');
+            setExpanded('expanded')
+            setEmailSpanText('Share via Email');
+        }
+        setToggleState(prevState => !prevState);
     }
 
     const emailHandler = (e) => {
@@ -20,11 +32,16 @@ const SharePostModal = ({ post, user, isVideo }) => {
 
     return (
         <div className='profile-post-container'>
-            <div onClick={sharePost} className='profile-post-div'>
-                <span>Send Email</span>
-                <input ref={emailInputRef} type='email' id='email-input' className={visibility} />
-                <button type='button' id='email-submit-button' onClick={emailHandler} className={visibility}></button>
+            <div  className='profile-share-div email-share-div'>
+                <span className={`email-span ${expanded}`} onClick={emailToggler}>{emailSpanText}</span>
+                <input ref={emailInputRef} type='email' className={`email-input ${visibility}`} aria-label='someone@email.com' placeholder='someone@email.com' />
+                <button type='button' className={`email-submit-button ${visibility}`} onClick={emailHandler}></button>
             </div>
+            {/* <div onClick={sharePost} className='profile-share-div' id='text-share-div'>
+                <span>Share via Email</span>
+                <input ref={emailInputRef} type='email' id='email-input' aria-label='email recipient here' placeholder='to ...@email.com' className={visibility} />
+                <button type='button' id='email-submit-button' onClick={emailHandler} className={visibility}></button>
+            </div> */}
             <div onClick={closeModal} className='profile-post-cancel'>Cancel</div>
         </div>
     )

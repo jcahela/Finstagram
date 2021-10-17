13887c28d82194b5a2c1638fa5c1bbc3f806711b
jasonc <jpacahela@gmail.com>
2021-10-17 10:49:48 -0500

    working on seeding users

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,14 +1,35 @@
 from app.models import db, User
+import random
+from faker import Faker
 
+fake = Faker()
+
+def get_profile_picture():
+    random_int = random.randint(1,99)
+    gender = ['men', 'women']
+    random_index = random.randint(0,1)
+    profile_picture_url = f'https://randomuser.me/api/portraits/med/{gender[random_index]}/{random_int}.jpg'
+    return profile_picture_url
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        firstname='Demo', lastname='User', username='Demo', email='demo@aa.io', password='password')
+        firstname='Demo', lastname='User', username='Demo', email='demo@aa.io', password='password', profile_picture=get_profile_picture())
+    for i in range(20):
+        new_user = User(
+            firstname=fake.name().split()[0],
+            lastname=fake.name().split()[1],
+            username=fake.username(),
+            email=fake.safe_email(),
+            password='password',
+            profile_picture=get_profile_picture()
+        )
+        db.session.add(new_user)
+        db.session.commit()
     marnie = User(
-        firstname='Marnie', lastname='Jones', username='marnie', email='marnie@aa.io', password='password')
+        firstname='Marnie', lastname='Jones', username='marnie', email='marnie@aa.io', password='password', profile_picture=get_profile_picture())
     bobbie = User(
-        firstname='Bobbie', lastname='Fischer', username='bobbie', email='bobbie@aa.io', password='password')
+        firstname='Bobbie', lastname='Fischer', username='bobbie', email='bobbie@aa.io', password='password', profile_picture=get_profile_picture())
 
     db.session.add(demo)
     db.session.add(marnie)
